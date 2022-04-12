1516762db835e51929e8c61515258ed48af5a107
Rahul <pahune21@gmail.com>
2022-04-12 14:05:14 +0530

    First commit using cmd

---
+++ src/test/java/Demo.java
@@ -0,0 +1,14 @@
+
+public class Demo {
+
+	public void m1() {
+		
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+   System.out.println("Hello Git");
+		Demo d=new Demo();
+		d.m1();
+	}
+
+}
